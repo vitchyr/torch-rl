96eefcda084cf800ff4b4f751e76be7e6c0cfd4b
Vitchyr Pong <vitchyr@gmail.com>
2016-01-15 15:48:28 -0500

    rename custom to threshold policities

--- test_thresholdpolicy.lua
+++ test_thresholdpolicy.lua
@@ -2,7 +2,7 @@ local easy21 = require 'easy21'
 require 'evaluation'
 require 'Agent'
 
-local cp = require 'custompolicies'
+local tp = require 'ThresholdPolicy'
 
 math.randomseed(os.time())
 
@@ -11,5 +11,5 @@ local e = Evaluator(a)
 for t = -1, 22 do
     e:displayMetrics(ThresholdPolicy(t), 'thresh, t = ' .. t)
 end
-e:displayMetrics(cp.always_hit, 'always hit')
-e:displayMetrics(cp.always_stick, 'always stick')
+e:displayMetrics(tp.always_hit, 'always hit')
+e:displayMetrics(tp.always_stick, 'always stick')
