ef30fb2e44008f3d123bede012ee1b9174e8382a
Vitchyr Pong <vitchyr@gmail.com>
2016-01-15 16:43:39 -0500

    only test one thing

--- unittest_agent.lua
+++ unittest_agent.lua
@@ -1,55 +1,35 @@
 require 'constants'
-local mdp = require 'mdp'
-local cp = require 'custompolicies'
+require 'Agent'
+require 'ThresholdPolicy'
+local easy21 = require 'easy21'
 
 local tester = torch.Tester()
 
-local TestMDP = {} --class
-    function TestMDP.test_alwayshit()
-        local episode = mdp.get_episode(cp.always_hit)
-        local data = episode[1]
-        local expected_Gt = -1
-        for t, data in pairs(episode) do
-            local s, a, Gt = table.unpack(data)
-            tester:asserteq(a, HIT)
-            tester:asserteq(Gt, expected_Gt)
-        end
-    end
-    function TestMDP.test_alwaysstick()
-        local episode = mdp.get_episode(cp.always_stick)
-        local data = episode[1]
-        local _, _, expected_Gt = table.unpack(data)
-        for t, data in pairs(episode) do
-            local s, a, Gt = table.unpack(data)
-            tester:asserteq(a, STICK)
-            -- Gt should be same for all actions since GAMMA = 1
-            if GAMMA == 1 then
-                tester:asserteq(Gt, expected_Gt)
-            end
-        end
-    end
-    function TestMDP.test_thresh()
-        for threshold = -1, 22 do
-            local episode = mdp.get_episode(cp.get_threshold_policy(threshold))
-            local data = episode[1]
-            local _, _, expected_Gt = table.unpack(data)
-            for t, data in pairs(episode) do
-                local s, a, Gt = table.unpack(data)
-                local dealer, player = table.unpack(s)
-                if player < threshold then
-                    tester:asserteq(a, HIT)
-                else
-                    tester:asserteq(a, STICK)
-                end
-                -- Gt should be same for all actions since GAMMA = 1
-                if GAMMA == 1 then
-                    tester:asserteq(Gt, expected_Gt)
-                end
-            end
-        end
-    end
--- class TestMDP
+local TestAgent = {}
 
-tester:add(TestMDP)
+local function get_threshold_episode(policy)
+    local a = Agent(easy21)
+    return a:get_episode(policy)
+end
+
+local function is_discount_good(episode)
+    local data = episode[1]
+    local _, _, last_Gt = table.unpack(data)
+    for t, data in pairs(episode) do
+        local s, a, Gt = table.unpack(data)
+        if GAMMA * Gt ~= last_Gt then
+            return false
+        end
+        last_Gt = Gt
+    end
+    return true
+end
+
+function TestAgent.test_get_episode_discounted_reward()
+    local episode = get_threshold_episode(ThresholdPolicy(12))
+    tester:assert(is_discount_good(episode))
+end
+
+tester:add(TestAgent)
 
 return tester
