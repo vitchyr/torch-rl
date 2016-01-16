30f78e3a8d1f67f7dc1e678aa2d860c3741253fe
Vitchyr Pong <vitchyr@gmail.com>
2016-01-15 20:20:34 -0500

    fix format

--- Evaluator.lua
+++ Evaluator.lua
@@ -1,29 +1,22 @@
 require 'constants'
 
-local M = {}
+local Evaluator = torch.class('Evaluator')
 
-do
-    local Evaluator = torch.class('Evaluator')
-
-    function Evaluator.__init(self, agent)
-        self.agent = agent
-    end
-
-    function Evaluator:getPolicyAvgReturn(policy, n_iters)
-        local total_r = 0
-        for i = 1, n_iters do
-            total_r = total_r + self.agent:sample_reward(policy)
-        end
-        return total_r
-    end
-
-    function Evaluator:displayMetrics(policy, description, n_iters)
-        n_iters = n_iters or N_ITERS
-        local totalR = self:getPolicyAvgReturn(policy, N_ITERS)
-        print('Avg Reward for ' .. description .. ': ' ..
-                totalR .. '/' .. N_ITERS .. ' = ' ..  totalR/N_ITERS)
-    end
+function Evaluator.__init(self, agent)
+    self.agent = agent
 end
-M.Evaluator = Evaluator
 
-return M
+function Evaluator:get_policy_avg_return(policy, n_iters)
+    local total_r = 0
+    for i = 1, n_iters do
+        total_r = total_r + self.agent:sample_reward(policy)
+    end
+    return total_r
+end
+
+function Evaluator:display_metrics(policy, description, n_iters)
+    n_iters = n_iters or N_ITERS
+    local total_r = self:get_policy_avg_return(policy, n_iters)
+    print('Avg Reward for <' .. description .. '>: ' ..
+            total_r .. '/' .. n_iters .. ' = ' ..  total_r/n_iters)
+end
--- test_thresholdpolicy.lua
+++ test_thresholdpolicy.lua
@@ -9,7 +9,7 @@ math.randomseed(os.time())
 local a = Agent(easy21)
 local e = Evaluator(a)
 for t = -1, 22 do
-    e:displayMetrics(ThresholdPolicy(t), 'thresh, t = ' .. t)
+    e:display_metrics(ThresholdPolicy(t), 'thresh, t = ' .. t)
 end
-e:displayMetrics(tp.always_hit, 'always hit')
-e:displayMetrics(tp.always_stick, 'always stick')
+e:display_metrics(tp.always_hit, 'always hit')
+e:display_metrics(tp.always_stick, 'always stick')
