2d2ada2af26b38afa56c45c836585e9fa7e87415
Vitchyr Pong <vitchyr@gmail.com>
2016-01-19 11:04:19 -0500

    fix names

--- LinSarsa.lua
+++ LinSarsa.lua
@@ -7,33 +7,33 @@ local fe = require 'featureextraction'
 
 -- Implement SARSA algorithm using a linear function approximator for on-line
 -- policy control
-local LS = torch.class('LinSarsa', 'Sarsa')
+local LinSarsa, parent = torch.class('LinSarsa', 'Sarsa')
 
-function TableSarsa:__init(mdp_config, policy, lambda, eps)
+function LinSarsa:__init(mdp_config, policy, lambda, eps)
     parent.__init(self, mdp_config, policy, lambda)
     eps = eps or EPS
     self.explorer = ConstExplorer(eps)
 end
 
-function LS:get_new_q()
+function LinSarsa:get_new_q()
     return q.QLin:new()
 end
 
-function LS:reset_eligibility()
+function LinSarsa:reset_eligibility()
     self.eligibility = q.QLin:new()
 end
 
-function LS:update_eligibility(s, a)
+function LinSarsa:update_eligibility(s, a)
     local features = fe.get_features(s, a)
     self.eligibility:mult(GAMMA*self.lambda)
     self.eligibility:add(features)
 end
 
-function LS:td_update(td_error)
+function LinSarsa:td_update(td_error)
     self.Q:add(self.eligibility:get_weight_vector() * self.alpha * td_error)
 end
 
-function LS:update_policy()
+function LinSarsa:update_policy()
     self.policy = GreedyPolicy(
         self.q,
         self.explorer,
--- NNSarsa.lua
+++ NNSarsa.lua
@@ -5,31 +5,31 @@ local fe = require 'featureextraction'
 
 -- Implement SARSA algorithm using a neural network function approximator for
 -- on-line policy control
-local NS = torch.class('NNSarsa', 'Sarsa')
+local NNSarsa = torch.class('NNSarsa', 'Sarsa')
 
-function TableSarsa:__init(mdp_config, policy, lambda, eps)
+function NNSarsa:__init(mdp_config, policy, lambda, eps)
     parent.__init(self, mdp_config, policy, lambda)
     eps = eps or EPS
     self.explorer = ConstExplorer(eps)
 end
 
-function NS:get_new_q()
+function NNSarsa:get_new_q()
     return q.QNN:new()
 end
 
-function NS:reset_eligibility()
+function NNSarsa:reset_eligibility()
 end
 
-function NS:update_eligibility(s, a)
+function NNSarsa:update_eligibility(s, a)
     self.last_s = s
     self.last_a = a
 end
 
-function NS:td_update(td_error)
+function NNSarsa:td_update(td_error)
     self.Q:backward(td_error, self.last_s, self.last_a, self.alpha, self.lambda)
 end
 
-function NS:update_policy()
+function NNSarsa:update_policy()
     self.policy = GreedyPolicy(
         self.q,
         self.explorer,
