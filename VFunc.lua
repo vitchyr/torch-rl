452e2b43aa1ad8070f97c1f0fc4f0917c651c946
Vitchyr Pong <vitchyr@gmail.com>
2016-01-19 14:01:36 -0500

    forgot to add VHash base class

---
+++ VFunc.lua
@@ -0,0 +1,20 @@
+util = require 'util'
+
+local VFunc = torch.class('VFunc')
+
+function VFunc:__init(mdp)
+    self.mdp = mdp
+end
+
+function VFunc:get_value(s)
+    error('Must implement get_Value method')
+end
+
+function VFunc:__eq(other)
+    for _, s in pairs(self.mdp:get_all_states()) do
+        if self:get_value(s) ~= other:get_value(s) then
+            return false
+        end
+    end
+    return true
+end
