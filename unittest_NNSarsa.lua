6fe58e96ac8b91073fb7223bb42c2e38f3c7415f
Vitchyr Pong <vitchyr@gmail.com>
2016-01-21 18:55:17 -0500

    fix test and add another one

--- unittest_NNSarsa.lua
+++ unittest_NNSarsa.lua
@@ -44,6 +44,53 @@ function TestNNSarsa.test_td_update_once()
     local grad_out = torch.Tensor{1}
     expected_module:backward(input, grad_out)
     local momentum = lambda * discount_factor
+    --expected_module:updateGradParameters(momentum, 0, false)
+    local learning_rate = step_size * td_error
+    expected_module:updateParameters(-learning_rate)
+
+    local expected_sarsa = NNSarsa(mdp_config, lambda, explorer, fe, step_size)
+    expected_sarsa.q.module = expected_module
+    expected_sarsa.last_state = s
+    expected_sarsa.last_action = a
+
+    tester:assert(sarsa == expected_sarsa)
+end
+
+function TestNNSarsa.test_td_update_many_times()
+    local lambda = 1
+    local eps = 0.032
+    local explorer = ConstExplorer(eps)
+    local step_size = 0.05
+    local sarsa = NNSarsa(mdp_config, lambda, explorer, fe, step_size)
+    local expected_module = sarsa.q.module:clone()
+
+    local s = 2
+    local a = 1
+    local td_error = -0.4
+    sarsa:update_eligibility(s, a)
+    sarsa:td_update(td_error)
+
+    local input = fe:get_sa_features(s, a)
+    expected_module:forward(input)
+    local grad_out = torch.Tensor{1}
+    expected_module:backward(input, grad_out)
+    local momentum = lambda * discount_factor
+    --expected_module:updateGradParameters(momentum, 0, false)
+    local learning_rate = step_size * td_error
+    expected_module:updateParameters(-learning_rate)
+
+    s = 2
+    a = 2
+    local td_error = -0.6
+    sarsa:update_eligibility(s, a)
+    sarsa:td_update(td_error)
+
+    input = fe:get_sa_features(s, a)
+    expected_module:forward(input)
+    grad_out = torch.Tensor{1}
+    expected_module:zeroGradParameters()
+    expected_module:backward(input, grad_out)
+    momentum = lambda * discount_factor
     expected_module:updateGradParameters(momentum, 0, false)
     local learning_rate = step_size * td_error
     expected_module:updateParameters(-learning_rate)
