7d36aac2c9e52d848113135b99a064a5d8c4579e
Vitchyr Pong <vitchyr@gmail.com>
2016-01-22 16:38:31 -0500

    remove 'results' word

--- analyze_table_sarsa.lua
+++ analyze_table_sarsa.lua
@@ -5,4 +5,4 @@ require 'constants'
 local as = require 'analyze_sarsa'
 
 local factory = TableSarsaFactory()
-as.analyze_sarsa(factory, 'table_sarsa_results')
+as.analyze_sarsa(factory, 'table_sarsa')
